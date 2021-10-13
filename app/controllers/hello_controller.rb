9e93d5d4144025c52455de19617024a4609d3278
okubomasaya <ooookubo.48@gmail.com>
2021-10-13 19:47:29 +0900

    controller

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,13 @@
+class HelloController < ApplicationController
+  def index
+    msg = "
+     <html>
+     <body>
+      <h1>Sample Page</h1>
+      <p>this is Rails sample page!</p>
+     </body>
+     </html> 
+    "
+    render html: msg.html_safe
+  end
+end
