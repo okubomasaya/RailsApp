463fa576af1985171a412947bb96b81028609373
okubomasaya <ooookubo.48@gmail.com>
2021-10-15 16:42:38 +0900

    input

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,13 +1,17 @@
 class HelloController < ApplicationController
+  # protect_from_forgery
+
   def index
-    msg = "
-     <html>
-     <body>
-      <h1>Sample Page</h1>
-      <p>this is Rails sample page!</p>
-     </body>
-     </html> 
-    "
-    render html: msg.html_safe
+    if request.post? then
+      @title = 'Result'
+      if params['r1'] then
+        @msg = 'you selected: ' + params['r1']
+      else
+        @msg = 'not selected...'
+      end
+    else
+      @title = 'Index'
+      @msg = 'selected radio button...'
+    end
   end
 end
